2ed78dcc1ceba553a0c7eec89edecea53979ee4c
XaviCanoGarcia <canogarciaxavi@gmail.com>
2024-03-16 02:37:16 +0100

    f

---
+++ src/main/java/fithub/clientEscriptori/Main.java
@@ -0,0 +1,10 @@
+package fithub.clientEscriptori;
+
+import fithub.clientEscriptori.app.ControladorAplicacio;
+
+public class Main {
+    public static void main(String[] args) {
+        ControladorAplicacio controladorAplicacio = new ControladorAplicacio();
+
+    }
+}
